dcd84e291b4b174af8c8e94b689060c4e4bbd87b
Sandinu Pinnawala <74131240+Sandinu@users.noreply.github.com>
2024-09-24 19:54:51 +0530

    Installing the initial libaries

---
+++ budget-buddy/src/app/_components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { SignedIn, SignInButton, SignedOut } from '@clerk/nextjs';
+import { UserButton } from '@clerk/nextjs';
+
+function Hero(){
+    return(
+        <div className="items-center text-center align-middle h-screen relative">
+            <SignedOut>
+                <SignInButton className='bg-neutral-300 py-2 px-3 mt-40'/>
+            </SignedOut>
+            <SignedIn>
+                <UserButton/>
+            </SignedIn>
+        </div>
+    )
+}
+
+export default Hero;
